dfe70cc470961c809c62a6ace1c6a21ab1e7f916
Samer Kanjo <samer@kanjo.net>
2024-04-08 10:31:38 -0500

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{liquid,md}", "./src/**/*.svg"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
+
