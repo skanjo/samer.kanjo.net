6469146b86b8ec96b54fa900ea4d654b09a87360
Samer Kanjo <samer@kanjo.net>
2024-11-29 22:54:37 -0600

    updated bluesky handle

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,7 +5,7 @@ module.exports = {
   githubRepoUrl: "https://github.com/skanjo/samer.kanjo.net",
   author: {
     name: "Samer Kanjo",
-    bluesky: "https://bsky.app/profile/samerkanjo.bsky.social",
+    bluesky: "https://bsky.app/profile/samer.kanjo.net",
     linkedin: "https://www.linkedin.com/in/samerkanjo",
     github: "https://github.com/skanjo",
     leetcode: "https://leetcode.com/u/samerkanjo/"
